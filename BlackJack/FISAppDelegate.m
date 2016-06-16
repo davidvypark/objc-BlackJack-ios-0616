b0e057fbefe7b930091fad13016e352e158d0873
davidvypark <davidvypark@gmail.com>
2016-06-15 21:22:58 -0400

    still need to do advanced section. suit symbols dont appear in console

--- BlackJack/FISAppDelegate.m
+++ BlackJack/FISAppDelegate.m
@@ -1,11 +1,26 @@
 //  FISAppDelegate.m
 
 #import "FISAppDelegate.h"
+#import "FISBlackjackGame.h"
 
 @implementation FISAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+//    FISBlackjackPlayer *davidPlayer = [[FISBlackjackPlayer alloc] initWithName:@"David"];
+//    
+//    FISCardDeck *newDeck = [[FISCardDeck alloc] init];
+//    [davidPlayer acceptCard:[newDeck drawNextCard]];
+//    
+//    NSLog(@"%@", davidPlayer.description);
+    
+    FISBlackjackGame *game = [[FISBlackjackGame alloc] init];
+    
+    [game playBlackjack];
+    
+    
+    
     return YES;
 }
 
